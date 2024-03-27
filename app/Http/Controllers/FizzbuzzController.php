00e888df32bb5267cdccd4a9475c8a41c39d12c3
sHKamil <shootekb@gmail.com>
2024-03-27 11:31:45 +0100

    FizzbuzzController refactor

--- app/Http/Controllers/FizzbuzzController.php
+++ app/Http/Controllers/FizzbuzzController.php
@@ -2,22 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\FizzbuzzService;
+use App\Services\FizzbuzzResponseService;
+use App\Validation\FizzbuzzArgumentValidator;
 use Illuminate\Http\Request;
 
 class FizzbuzzController extends Controller
 {
-    public function index($number = null)
+    public function index($argument = null)
     {
-        $fizzBuzz = new FizzbuzzService;
-        $response = [];
-        if($number !== null && is_numeric($number)){
-            $response = [$number => is_numeric($fizzBuzz->associateFizzBuzz($number)) ? '' : $fizzBuzz->associateFizzBuzz($number)];
-        } else {
-            for ($i=1; $i <= 100; $i++) { 
-                array_push($response, [$i => is_numeric($fizzBuzz->associateFizzBuzz($i)) ? '' : $fizzBuzz->associateFizzBuzz($i)]);
-            }
+        if (!FizzbuzzArgumentValidator::validate($argument)) {
+            return response()->json(['error' => 'The argument must be numeric.']);
         }
+
+        $response = FizzbuzzResponseService::getResponse($argument);
         return response()->json($response);
     }
 }
