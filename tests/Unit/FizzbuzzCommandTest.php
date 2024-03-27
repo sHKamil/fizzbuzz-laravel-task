e7a72e0acb06db5c3c0c3e3dcf01b112121dd9f5
sHKamil <shootekb@gmail.com>
2024-03-27 11:44:34 +0100

    Unit test for FizzBuzz command

---
+++ tests/Unit/FizzbuzzCommandTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit\Console\Commands;
+
+use Tests\TestCase;
+
+class FizzbuzzCommandTest extends TestCase
+{
+    /** @test */
+    public function test_command_returns_string_fizz()
+    {
+        // Execute the command with the argument '3'
+        $output = $this->artisan('fizzbuzz', ['number' => '3']);
+
+        // Assert that the output is 'Fizz'
+        $output->expectsOutput('Fizz');
+    }
+
+    /** @test */
+    public function test_command_returns_string_buzz()
+    {
+        // Execute the command with the argument '5'
+        $output = $this->artisan('fizzbuzz', ['number' => '5']);
+
+        // Assert that the output is 'Buzz'
+        $output->expectsOutput('Buzz');
+    }
+
+    /** @test */
+    public function test_command_returns_string_fizzbuzz()
+    {
+        // Execute the command with the argument '15'
+        $output = $this->artisan('fizzbuzz', ['number' => '15']);
+
+        // Assert that the output is 'FizzBuzz'
+        $output->expectsOutput('FizzBuzz');
+    }
+}
